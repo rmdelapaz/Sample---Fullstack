01403a601637932d3a4e7aca728348bfd77a87ae
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-03-08 20:46:39 -0800

    Prepare backend for deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,26 +1,39 @@
-
-// backend/routes/index.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const apiRouter = require('./api');
+const apiRouter = require("./api");
 
-router.use('/api', apiRouter);
+router.use("/api", apiRouter);
 
+// Serve React build files in production
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
 
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
+  // Serve the frontend's index.html file at the root route
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "dist", "index.html")
+    );
   });
-});
 
+  // Serve static assets
+  router.use(express.static(path.resolve("../frontend/dist")));
 
-// const { Spot } = require('../db/models');
-// router.get("/api/spots", async (req, res) => {
-//   const spots = await Spot.findAll(); // Fetch all spots
-//   res.status(200).json(spots);
-// })
+  // Serve index.html at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "dist", "index.html")
+    );
+  });
+}
+
+// Restore CSRF token in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.status(200).json({ "XSRF-Token": req.csrfToken() });
+  });
+}
 
 module.exports = router;
