7c93948574c07dc46ec0f76e97b25c3691e80339
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-03-10 15:32:14 -0700

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 
 const store = configureStore();
@@ -13,12 +14,19 @@ if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+//Run restoreUser on app startup (preload session)
+async function initializeApp() {
+  await store.dispatch(sessionActions.restoreUser());
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+initializeApp();
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,17 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import sessionReducer from "./session";
+import logger from "redux-logger";
 
 const rootReducer = combineReducers({
   // ADD REDUCERS HERE
+  session: sessionReducer,
 });
 
 let enhancer;
 if (import.meta.env.MODE === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = (await import("redux-logger")).default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
