4b2da30e69d8c8b1e0ffaff23671d81dda060a52
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-03-13 18:03:09 -0700

    updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,11 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
+router.get("/api/csrf/restore", (req, res) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  return res.json({});
+});
+
 // Serve React build files in production
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
